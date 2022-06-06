5f2365af6ae9ae23a9a9b0675753134390691f06
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-06-06 15:01:04 -0600

    Heroku

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = require('./app');
 mongoose.Promise = global.Promise;
 
 mongoose.Promise = global.Promise;                                                                  
-mongoose.connect('mongodb://localhost:27017/TorneoDeportivo', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.knu6h7m.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Conectado a la base de datos.");
     usuarioControler.adminInicio();
 
-    app.listen(3000, function () {
+    app.listen(process.env.PORT || 3000, function () {
         console.log("Corriendo en el puerto 3000!")
     })
 
